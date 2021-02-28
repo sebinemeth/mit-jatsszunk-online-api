677757d9d75846fe3f07f4850c11fba403eb9d36
Sebi Nemeth <sebi.nemeth@apicore.de>
2021-02-28 14:26:46 +0100

    entry endpoints

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,14 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
-import { addEntry } from "./entryController";
+import { addEntry, getAllEntries, updateEntry, deleteEntry } from "./entryController";
 
 const api = express.Router();
 
 api.get("/", (req, res) => res.status(200).send("mitjatsszunk.online api"));
 api.post("/entries", addEntry);
+api.get("/entries", getAllEntries);
+api.patch('/entries/:entryId', updateEntry)
+api.delete('/entries/:entryId', deleteEntry)
 
 const app = express();
 app.use("/api", api);
