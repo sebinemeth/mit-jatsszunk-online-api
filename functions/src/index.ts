85894d8c744df2f0c12a5d7e3e9ae37edbd99eb1
Sebi Nemeth <sebi.nemeth@apicore.de>
2021-02-28 13:58:22 +0100

    fix export name

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,9 @@ import * as functions from 'firebase-functions'
 import * as express from 'express'
 import { addEntry } from './entryController'
 
-const api = express()
+const app = express()
 
-api.get('/', (req, res) => res.status(200).send('mitjatsszunk.online api'))
-api.post('/entries', addEntry)
+app.get('/', (req, res) => res.status(200).send('mitjatsszunk.online api'))
+app.post('/entries', addEntry)
 
-exports.app = functions.https.onRequest(api)
+exports.api = functions.https.onRequest(app)
