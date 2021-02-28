2013d6c2e20ef41e087ac84295c7a154bb613d1e
Sebi Nemeth <sebi.nemeth@apicore.de>
2021-02-28 13:37:51 +0100

    entry post

---
+++ functions/src/entryController.ts
@@ -0,0 +1,35 @@
+import { Response } from "express";
+import { db } from "./config/firebase";
+
+type EntryType = {
+  title: string;
+  text: string;
+  coverImageUrl: string;
+};
+
+type Request = {
+  body: EntryType;
+  params: { entryId: string };
+};
+
+export const addEntry = async (req: Request, res: Response) => {
+  const { title, text } = req.body;
+  try {
+    const entry = db.collection("entries").doc();
+    const entryObject = {
+      id: entry.id,
+      title,
+      text
+    };
+
+    entry.set(entryObject);
+
+    res.status(200).send({
+      status: "success",
+      message: "entry added successfully",
+      data: entryObject
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
+import { addEntry } from './entryController'
 
 const app = express()
+
 app.get('/', (req, res) => res.status(200).send('Hey there!'))
+app.post('/entries', addEntry)
+
 exports.app = functions.https.onRequest(app)
