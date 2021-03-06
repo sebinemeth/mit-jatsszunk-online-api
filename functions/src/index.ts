fb3b3750272333f04bb601f9478b521d46d38b61
Sebi Nemeth <sebi.nemeth@apicore.de>
2021-03-07 00:18:12 +0100

    local development

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,19 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
-import { addEntry, getAllEntries, updateEntry, deleteEntry } from "./entryController";
+import {
+  addEntry,
+  getAllEntries,
+  updateEntry,
+  deleteEntry
+} from "./entryController";
 
 const api = express.Router();
 
 api.get("/", (req, res) => res.status(200).send("mitjatsszunk.online api"));
 api.post("/entries", addEntry);
 api.get("/entries", getAllEntries);
-api.patch('/entries/:entryId', updateEntry)
-api.delete('/entries/:entryId', deleteEntry)
+api.patch("/entries/:entryId", updateEntry);
+api.delete("/entries/:entryId", deleteEntry);
 
 const app = express();
 app.use("/api", api);
